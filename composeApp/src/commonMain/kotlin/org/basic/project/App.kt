a8b9f5a95c8f096242b340212344fc1da949074b
AugustoGiuffrida <a.giuffrida@alumno.um.edu.ar>
2025-08-31 23:46:00 -0300

    Second commit

--- composeApp/src/commonMain/kotlin/org/basic/project/App.kt
+++ composeApp/src/commonMain/kotlin/org/basic/project/App.kt
@@ -5,15 +5,20 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
@@ -23,6 +28,10 @@ import kmpbasico.composeapp.generated.resources.compose_multiplatform
 @Composable
 fun App() {
     MaterialTheme {
+        var name: String by remember { mutableStateOf("name") }
+        var password: String by remember { mutableStateOf("password") }
+
+
         Column (
                 modifier = Modifier
                 .background(MaterialTheme.colorScheme.primaryContainer)
@@ -30,9 +39,25 @@ fun App() {
                 .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally)
         {
-            Text(text = "Hola mundo!!")
-            Text(text = "Programcion 2")
-            Text(text = "AndroidStudio")
+            TextField(value = name, onValueChange = {name = it})
+
+
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AnimatedVisibility(name.isNotEmpty()){
+                Text(text = "Version animada: $name", fontSize = 20.sp)
+            }
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            TextField(value = password, onValueChange = {password = it})
+
+            Spacer(modifier = Modifier.height(40.dp))
+
+            AnimatedVisibility(password.isNotEmpty()){
+                Text(text = "Version animada: $password", fontSize = 20.sp)
+            }
         }
     }
 }
